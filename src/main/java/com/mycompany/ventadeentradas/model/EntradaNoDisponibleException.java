39dabf906e719c8bcf7275eee57d99bf553adaa4
floowxy <alejsot1234@gmail.com>
2026-05-22 11:33:00 -0500

    add exception EntradaNoDisponible

---
+++ src/main/java/com/mycompany/ventadeentradas/model/EntradaNoDisponibleException.java
@@ -0,0 +1,8 @@
+package com.mycompany.ventadeentradas.model;
+
+public class EntradaNoDisponibleException extends Exception {
+
+    public EntradaNoDisponibleException(String message) {
+        super(message);
+    }
+}
